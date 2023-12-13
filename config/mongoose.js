4b9d2e2f8066076cab27fb7580f8ebe513342333
AmanTiwari <er.amantiwari.2002@gmail.com>
2023-12-13 13:46:30 +0530

    Mongo DB URI is Local to the OS

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-const MONGO_URL = "mongodb+srv://pawnalakeholiday:%23PawnaLake123@pawnalake.apaxdtd.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URL = "mongodb://127.0.0.1/pawnalake_db"
 
 mongoose.connect(MONGO_URL);
 
